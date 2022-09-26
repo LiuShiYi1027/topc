239e8bc9988eb988194163a74af68e7964963518
LiuShiYi <1181107503@qq.com>
2022-09-26 13:09:17 +0000

    add cli version

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/LiuShiYi1027/topc/version"
+	"github.com/spf13/cobra"
+)
 
 func main() {
-	fmt.Println("hello ctop")
+	cmd := newTopcCommand()
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func newTopcCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "topc [OPTIONS] COMMAND [ARG...]",
+		Short:   "A performance monitor for container",
+		Version: fmt.Sprintf("%s, build %s", version.Version, version.GitCommit),
+	}
+
+	cmd.AddCommand(newVersionCommand())
+	cmd.AddCommand(newMemCommand())
+
+	return cmd
+}
+
+func newVersionCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of TopC",
+		Long:  `All software has versions. This is TopC's`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("v%s -- HEAD\n", version.Version)
+		},
+	}
+}
+
+func newMemCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "memory",
+		Short: "Print the memory info of current container",
+		Run: func(cmd *cobra.Command, args []string) {
+			
+		},
+	}
 }
---
+++ version/version.go
@@ -0,0 +1,6 @@
+package version
+
+var (
+	Version   string = "0.0.1"
+	GitCommit string = "init"
+)
